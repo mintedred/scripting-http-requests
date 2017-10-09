9ce040ea34b4bec29c945defcf18524610d09479
Corina Wong <hi@corinawong.com>
2017-10-09 15:51:44 +0000

    Make it modular

---
+++ http-functions.js
@@ -0,0 +1,15 @@
+module.exports = function getHTML(options, callback) { 
+  var https = require('https');  
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      data += data;
+      callback(data);
+    });
+
+    response.on('end', function() {
+      console.log('Response stsream complete.')
+    })
+  });
+};
