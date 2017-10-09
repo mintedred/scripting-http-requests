a4c9058a17ca0807852b00f4e2c1cc3275271f73
Corina Wong <hi@corinawong.com>
2017-10-09 15:28:59 +0000

    Add callback function

---
+++ gethtml.js
@@ -0,0 +1,32 @@
+var https = require('https');
+
+
+function getHTML (options, callback) {
+  
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      var recievedData;
+      recievedData += data;
+
+      callback(data);
+    });
+
+    response.on('end', function() {
+      console.log('Response stsream complete.')
+    })
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
