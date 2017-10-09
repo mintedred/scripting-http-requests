309a327fd8ad4f97cfa3b2c40fa39c96d9903b78
Corina Wong <hi@corinawong.com>
2017-10-09 16:11:31 +0000

    Reverse HTML string

---
+++ callbacks/print_reverse.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+
+function reverse(html) {
+  console.log(html.split("").reverse().join(""));
+}
+
+getHTML(requestOptions, reverse);
